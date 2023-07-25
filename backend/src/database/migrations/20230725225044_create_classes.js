a99873d34388f27851262fb1b840b126f53376c2
Fernando Fonseca <ffonseca1986@gmail.com>
2023-07-25 20:03:58 -0300

    pi-001-configured knex and create teachers/classes tables

---
+++ backend/src/database/migrations/20230725225044_create_classes.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('classes', function (table){
+        table.increments();
+        
+        table.string('title').notNullable();
+        table.string('description').notNullable();
+        table.decimal('value').notNullable();
+
+        table.string('teacher_id').notNullable();
+
+        table.foreign('teacher_id').references('id').inTable('teachers');
+      });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTable('classes');
+};
