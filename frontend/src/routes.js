cd4036edbd39abc825d597ec332372b7c3de75e6
Fernando Fonseca <ffonseca1986@gmail.com>
2023-08-09 20:39:28 -0300

    pi-005-created-profile-and-newclasses-pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -2,6 +2,8 @@ import  {BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Logon from './pages/logon';
 import Register from './pages/register';
+import Profile from './pages/profile';
+import NewClasses from './pages/newClasses';
 
 export default function Routers() {
     return (
@@ -9,6 +11,9 @@ export default function Routers() {
             <Routes>
                 <Route path='/login' element={<Logon />} />
                 <Route path='/register' element={<Register />} />
+
+                <Route path ='/profile' element={<Profile />} />
+                <Route path ='/classes/new' element={<NewClasses />} />
             </Routes>
         </BrowserRouter>
     )
